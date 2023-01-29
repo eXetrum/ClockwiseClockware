ab732c871fbef384179c9c42f56f07261b6722c8
eXetrum <shishimorovmaksim@gmail.com>
2023-01-29 11:44:23 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+const { pool } = require("./db");
+
 app.get('/', (req, res) => {
-	res.send('Hello World!')
+	res.send('Hello World!');
+	//pool
 })
 
 app.listen(port, () => {
