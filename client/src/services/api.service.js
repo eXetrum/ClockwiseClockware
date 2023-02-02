c618da46bcba3402ea2d38a6b0aec6eb217ec044
eXetrum <shishimorovmaksim@gmail.com>
2023-02-02 15:02:24 +0200

    client env

--- client/src/services/api.service.js
+++ client/src/services/api.service.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:4200/api";
+const API_URL = process.env.REACT_APP_API_URL;
+//"http://localhost:4200/api";
 
 class ApiService {
     constructor() {
--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import jwt from 'jwt-decode';
 
-const API_URL = "http://localhost:4200/api";
+const API_URL = process.env.REACT_APP_API_URL;
 
 class AuthService {
   login(email, password) {
