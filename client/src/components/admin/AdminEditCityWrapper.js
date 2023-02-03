e9c103ca41491bfa7f3323db07009ca234e49b0f
eXetrum <shishimorovmaksim@gmail.com>
2023-02-03 15:55:24 +0200

    Delete AdminEditCityWrapper.js

--- client/src/components/admin/AdminEditCityWrapper.js
+++
@@ -1,9 +0,0 @@
-import { useParams } from 'react-router-dom';
-import AdminEditCity from './AdminEditCity';
-
-const AdminEditCityWrapper = () => {
-    const {id} = useParams();
-    return <AdminEditCity id={id} />;
-};
-
-export default AdminEditCityWrapper;
