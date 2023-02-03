27e24c4d245dfc1fe0f44926209121de215985e7
eXetrum <shishimorovmaksim@gmail.com>
2023-02-03 16:13:15 +0200

    Update AdminEditMaster.js

--- client/src/components/admin/AdminEditMaster.js
+++ client/src/components/admin/AdminEditMaster.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
-import {Form, FormGroup, FormControl, Container, Row, Col, Button, Alert} from 'react-bootstrap';
+import {
+    Form, FormGroup, FormControl, Container, Row, Col, Button, Alert, Spinner
+} from 'react-bootstrap';
 import StarRating from '../StarRating';
 import Header from '../Header';
 import ApiService from '../../services/api.service';
@@ -22,69 +24,82 @@ const AdminEditMaster = () => {
         console.log('useEffect');
         const getCities = async () => {
             try {
-
+                const response = await ApiService.getCities();
+                if(response && response.data && response.data.cities) {
+                    const { cities } = response.data;
+                    setCities(cities);
+                    let curCityCheck = [];
+                    cities.forEach((item, index) => { curCityCheck[item.id] = false; });
+                    setCityCheck(curCityCheck);
+                }
             } catch(e) {
                 setError(e);
+            } finally {
+                setPending(false);
             }
         };
-        ApiService.getCities()
-        .then(response => {
-            console.log('ApiService.getCities(): ', response.data.cities);
-            if(response && response.data) {
-                setCities(response.data.cities);
-                let cityCheck = {};
-                response.data.cities.map((item, index) => { cityCheck[item.id] = false; });
-                setCityCheck(cityCheck);
+        
+        setPending(true);
+        getCities();
+    }, []);
+
+    useEffect(() => {
+        const getMasterById = async (id) => {
+            try {
+                const response = await ApiService.getMasterById(id);
+                if(response && response.data && response.data.master) {
+                    const { master } = response.data;
+                    let curCityCheck = [];
+                    master.cities.forEach((item, index) => { curCityCheck[item.id] = true; });
+                    setCityCheck(curCityCheck);
+                    setMaster(master);
+                }
+            } catch(e) {
+                setError(e);
+            } finally {
+                setPending(false);
             }
-        },
-        error => { setError(error); });
-        ApiService.getMasterById(id)
-        .then(response => {
-            console.log('ApiService.getMasterById(): ', response.data.master);
-            if(response && response.data) {
-                const { master } = response.data;
-                master.cities.map((item, index) => { cityCheck[item.id] = true; });
-                setCityCheck(cityCheck);
-                setMaster(master);
-            }
-        },
-        error => { setError(error); });
+        };
+
+        setPending(true);
+        getMasterById(id);
     }, []);
 
 	// Callbacks
 	const handleSubmit = (e) => {
 		e.preventDefault()
-        console.log('submit: ', master);
-        // Reset to default
-		cities.map((item, index) => { cityCheck[item.id] = false; });
+        // Reset to default		
+        const updateMasterById = async (id, master) => {
+            try {
+                const response = await ApiService.updateMasterById(id, master);
+                if(response && response.data && response.data.master) {
+                    const { master } = response.data;
+                    let curCityCheck = [];
+                    master.cities.forEach((item, index) => { curCityCheck[item.id] = true; });
+                    setCityCheck(curCityCheck);
+                    setMaster(master);
+                    setInfo('success');
+                }
+            } catch(e) {
+                setError(e);
+            } finally {
+                setPending(false);
+            }
+        };
+
+        setPending(true);
+        updateMasterById(id, master);
+
+        cities.map((item, index) => { cityCheck[item.id] = false; });
 		setCityCheck(cityCheck);
 		setMaster({ name: '', email: '', rating: 0, cities: [] });
-		setInfo('');
-		setError('');
-		//const oldCities = structuredClone(cities);
-		//setCities([]);
-        ApiService.updateMasterById(id, master)
-        .then(response => {
-            if(response && response.data) {
-				const { master } = response.data;
-                console.log('received: ', master);
-                master.cities.map((item, index) => { cityCheck[item.id] = true; });
-                setCityCheck(cityCheck);
-                setMaster(master);
-				setInfo('success');
-				//setCities(oldCities);
-            }
-        }, error => {
-            setError(error);
-            //setCities(oldCities);
-        });
+        
+		setInfo(null);
+		setError(null);
 	};
 
 
     const validateNewMasterForm = () => { return !master.name || !master.email || !master.cities.length; };
-
-	
-
 
 	// 'render'
     return (
@@ -94,7 +109,10 @@ const AdminEditMaster = () => {
 				<center>
 					<h1>Admin: Edit Master</h1>
 				</center>
-				<Row className="justify-content-md-center">
+                <hr/>
+                {(!master && pending) && <center><Spinner animation="grow" /> </center>}
+				{master &&
+                <Row className="justify-content-md-center">
                 	<Col xs>
                     <Form inline="true" className="d-flex align-items-end" onSubmit={handleSubmit}>
                         <FormGroup>
@@ -157,18 +175,20 @@ const AdminEditMaster = () => {
                             }
                         </FormGroup>
 
-                        <Button type="submit" disabled={validateNewMasterForm()} >Update</Button>
+                        <Button type="submit" className="ms-2" disabled={validateNewMasterForm()} >Update</Button>
                     </Form>
                 	</Col>
               	</Row>
+                }
 
             <hr/>
 			<Row className="justify-content-md-center">
                 <Col md="auto">
                     {info && <Alert key='success' variant='success'>{info}</Alert>}
-                    {error && <Alert key='danger' variant='danger'>{error}</Alert>}
+                    {error && <Alert key='danger' variant='danger'>{error.toString()}</Alert>}
                 </Col>
             </Row>
+            
 			</Container>
 		</Container>
     );
