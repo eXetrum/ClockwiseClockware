64e2afe10107d5e843696fbb65eaae0dcfb787a2
eXetrum <shishimorovmaksim@gmail.com>
2023-02-03 11:34:57 +0200

    erase oldstyle component

--- client/src/components/admin/AdminEditMasterWrapper.js
+++
@@ -1,8 +0,0 @@
-import { useParams } from 'react-router-dom';
-
-const AdminEditMasterWrapper = () => {
-    const {id} = useParams();
-    return <AdminEditMaster id={id} />;
-};
-
-export default AdminEditMasterWrapper;
