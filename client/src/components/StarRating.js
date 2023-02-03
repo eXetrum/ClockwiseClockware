c4aa599ad5baf22934122dbc2592299eacf8896a
eXetrum <shishimorovmaksim@gmail.com>
2023-02-03 15:55:18 +0200

    star rating font size adjust

--- client/src/components/StarRating.js
+++ client/src/components/StarRating.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import './StarRating.css';
 
-const StarRating = ({total, value, onRatingChange, onRatingReset}) => {
+const StarRating = ({total, value, readonly=false, onRatingChange=null, onRatingReset=null}) => {
     const [rating, setRating] = useState(value);
     const [hover, setHover] = useState(value);
-    useEffect(() => { setRating(value) }, [value]);
+    useEffect(() => { setRating(value); }, [value]);
+	console.log('readonly:',readonly);
     return (
 		<div className="star-rating" >
 			{[...Array(total)].map((star, index) => {
@@ -15,20 +16,28 @@ const StarRating = ({total, value, onRatingChange, onRatingReset}) => {
 					key={index}					
 					className={index <= ((rating && hover) || rating || hover) ? "on" : "off"}
 					onClick={() => {
+						if(readonly) return;
 						setRating(index); 
 						if(onRatingChange) {
 							onRatingChange(index); 
 						}
 					}}
 					onDoubleClick={() => {
+						if(readonly) return;
 						setRating(0);
 						setHover(0);
 						if(onRatingReset) {
 							onRatingReset(0);
 						}
 					}}
-					onMouseEnter={() => setHover(index)}
-					onMouseLeave={() => setHover(rating)}
+					onMouseEnter={() => {
+						if(readonly) return;
+						setHover(index)
+					}}
+					onMouseLeave={() => {
+						if(readonly) return;
+						setHover(rating)
+					}}
 					>
 					<span className="star">&#9733;</span>
 					</button>
