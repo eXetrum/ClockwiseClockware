19c7796508520d87bda2fe2c11dc0c68ee04f2b8
eXetrum <shishimorovmaksim@gmail.com>
2023-02-09 12:45:52 +0200

    Now Header links is actually route links

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
 import { Nav, Navbar, NavDropdown, Container } from 'react-bootstrap';
 import { getCurrentUser } from "../api/auth";
 
@@ -13,42 +14,39 @@ const Header = () => {
     return (
     <Navbar bg="light" variant="light" className="mb-3">
         <Container>
-        <Navbar.Brand href="/">Clockwise Clockware</Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-auto">
-        
-        {user ? (
-        <Nav>
-            <Nav.Link href="/admin/cities">Cities</Nav.Link>
-            <Nav.Link href="/admin/masters">Masters</Nav.Link>
-            <Nav.Link href="/admin/clients">Clients</Nav.Link>            
-            <Nav.Link href="/admin/orders">Orders</Nav.Link>
-        </Nav>
-        ):("")}
-        </Navbar.Collapse>
+            <Navbar.Brand as={Link} to="/">Clockwise Clockware</Navbar.Brand>
+            <Navbar.Toggle />
+                <Navbar.Collapse className="justify-content-auto">        
+                    {user && (   
+                    <Nav>
+                        <Nav.Link as={Link} to="/admin/cities">Cities</Nav.Link>
+                        <Nav.Link as={Link} to="/admin/masters">Masters</Nav.Link>
+                        <Nav.Link as={Link} to="/admin/clients">Clients</Nav.Link>
+                        <Nav.Link as={Link} to="/admin/orders"> Orders</Nav.Link>
+                    </Nav>)}
+                </Navbar.Collapse>
+            <Navbar.Toggle />
 
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-            <Navbar.Text>
-            </Navbar.Text>
-            {user ? (
-            <>
-                <Nav.Link href="/order" className="me-3">Order</Nav.Link>
-                <NavDropdown title={user.email} id="basic-nav-dropdown">
-                    
-                    <NavDropdown.Item href="/profile">Profile</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="/logout">
-                        Logout
-                    </NavDropdown.Item>
-                </NavDropdown>
-            </>
-            ) : (<>
-                    <Nav.Link href="/order" className="me-3">Order</Nav.Link>
-                    <Nav.Link href="/login">Login</Nav.Link>
-                </>)
-            }
-        </Navbar.Collapse>
+            <Navbar.Collapse className="justify-content-end">
+                <Navbar.Text>
+                </Navbar.Text>
+                {user ? (
+                    <Nav>
+                        <Nav.Link as={Link} to="/order" className="me-3">Order</Nav.Link>
+                        <NavDropdown title={user.email} id="basic-nav-dropdown">                            
+                            <NavDropdown.Item as={Link} to="/profile">Profile</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item as={Link} to="/logout">
+                                Logout
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                ) : (<Nav>
+                        <Nav.Link as={Link} to="/order" className="me-3">Order</Nav.Link>
+                        <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                    </Nav>)
+                }
+            </Navbar.Collapse>
         </Container>
     </Navbar>
     );
