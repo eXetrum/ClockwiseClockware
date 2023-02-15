affba64ab3a2277ca89fbb0e8940a8d00d1ef219
eXetrum <shishimorovmaksim@gmail.com>
2023-02-15 12:26:40 +0200

    update orders list layout

--- client/src/components/admin/AdminOrdersList.js
+++ client/src/components/admin/AdminOrdersList.js
@@ -1,88 +1,139 @@
 import React, { useState, useEffect} from 'react';
 import { Link }  from 'react-router-dom';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import EditIcon from '@mui/icons-material/Edit';
 
 import {
-    Row, Col, Table,  Alert
+    Row, Col, Table,  Alert, Container, Card, Badge
 } from 'react-bootstrap';
 import StarRating from '../StarRating';
 
 const AdminOrdersList = ({orders, onRemove=null}) => {
-    
-    // Initial
-	const [_orders, setOrders] = useState(null);
-    useEffect(() => { setOrders(orders); }, [orders]);
+    const formatDate = (date) => {
+        const padTo2Digits = (num) => { return num.toString().padStart(2, '0'); };
+        return (
+            [
+            date.getFullYear(),
+            padTo2Digits(date.getMonth() + 1),
+            padTo2Digits(date.getDate()),
+            ].join('-') +
+            ' ' +
+            [
+            padTo2Digits(date.getHours()),
+            padTo2Digits(date.getMinutes()),
+            ].join(':')
+        );
+    };
+
+    const [curDate, setCurDate] = useState(new Date());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurDate(new Date());
+        }, 5000);
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
 
     return (
         <>
-            {_orders && _orders.length === 0 && 
+            {orders && orders.length === 0 && 
             <Row className="justify-content-md-center">
                 <Col md="auto">
                     <Alert>No records yet</Alert>
                 </Col>
             </Row>
             }
-            {_orders && _orders.length > 0 && 
-            <Table striped bordered responsive size="sm" className="mt-3">
-                <thead>
-                    <tr>
-                        <th className="text-center p-2 m-0">id</th>
-                        
-                        <th className="text-center p-2 m-0">client name</th>
-                        <th className="text-center p-2 m-0">client email</th>
+            {orders && orders.length > 0 && 
+            <>
+                <Row>
+                    <Col className="text-center p-2 m-0 col-1"><strong>id</strong></Col>
+                    <Col className="text-center p-2 m-0 col-3"><strong>Client</strong></Col>
+                    <Col className="text-center p-2 m-0 col-3"><strong>Master</strong></Col>
 
-                        <th className="text-center p-2 m-0">master name</th>
-                        <th className="text-center p-2 m-0">master email</th>
-                        <th className="text-center p-2 m-0">master rating</th>
+                    <Col className="text-center p-2 m-0 col-1"><strong>City</strong></Col>
+                    
+                    <Col className="text-center p-2 m-0 col-1"><strong>Clock</strong></Col>
+                    
+                    <Col className="text-center p-2 m-0 col-2"><strong>Date</strong></Col>
+                    
+                    <Col className="text-center p-2 m-0 col-1">&nbsp;</Col>
 
-                        <th className="text-center p-2 m-0">city name</th>
-                        
-                        <th className="text-center p-2 m-0">watch type name</th>
-                        <th className="text-center p-2 m-0">watch repair_time (hours)</th>
-                        
-                        <th className="text-center p-2 m-0">date start</th>
-                        <th className="text-center p-2 m-0">date end</th>
-                        
-                        <th colSpan="2" className="text-center p-2 m-0"></th>
-                    </tr>
-                </thead>
-                <tbody>
-                {_orders && _orders.map((order, index) => {
+                </Row>
+                {orders.map((order, index) => {
                     return (
-                    <tr key={"order_id_" + order.id}>
-                        <td className="text-center p-2 m-0">{ order.id }</td>
-                        
-                        <td className="text-center p-2 m-0">{ order.client.name }</td>
-                        <td className="text-center p-2 m-0">{ order.client.email }</td>
-                        
-                        <td className="text-center p-2 m-0">{ order.master.name }</td>
-                        <td className="text-center p-2 m-0">{ order.master.email }</td>
-                        <td className="text-center p-2 m-0"><StarRating value={order.master.rating} readonly={true} /></td>
-                        
-                        <td className="text-center p-2 m-0">{ order.city.name }</td>
+                    <div key={"order_id_" + order.id}>
+                        <Row>
+                            <Col className="text-center p-2 m-0 col-1">{ order.id }</Col>
+                            
+                            <Col className="text-center p-2 m-0 col-3">
+                                <Card>
+                                    <Card.Header><Card.Title>{ order.client.name }</Card.Title></Card.Header>
+                                    <Card.Body>                                    
+                                        <small className="text-muted">
+                                            <a className="text-muted" href={`mailto:${order.client.email}`} target="_blank" rel="noreferrer noopener">
+                                                <EmailOutlinedIcon />
+                                            </a>
+                                            { order.client.email }
+                                        </small>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                            
+                            <Col className="text-center p-2 m-0 col-3">
+                                <Card>
+                                    <Card.Header><Card.Title>{ order.master.name }</Card.Title></Card.Header>
+                                    <Card.Body>                                    
+                                        <small className="text-muted">
+                                            <a className="text-muted" href={`mailto:${order.master.email}`} target="_blank" rel="noreferrer noopener">
+                                                <EmailOutlinedIcon />
+                                            </a>
+                                            { order.master.email }
+                                        </small>
+                                        <StarRating value={order.master.rating} readonly={true} />
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                            
+                            <Col className="text-center p-2 m-0 col-1"><Badge className="p-2">{ order.city.name }</Badge></Col>
 
-                        <td className="text-center p-2 m-0">{ order.watchType.name }</td>
-                        <td className="text-center p-2 m-0">{ order.watchType.repairTime }</td>
-                        <td className="text-center p-2 m-0">{ order.dateTime.startDate }</td>
-                        <td className="text-center p-2 m-0">{ order.dateTime.endDate }</td>
+                            <Col className="text-center p-2 m-0 col-1">
+                                <Badge className="p-2" bg="info">{ order.watchType.name }
+                                    <Badge pill bg="secondary" className="ms-2">{ order.watchType.repairTime }h</Badge>
+                                </Badge>
+                            </Col>
 
-                        <td className="text-center p-2 m-0">
-                            <Link to={"/admin/orders/" + order.id} >
-                                <EditIcon />
-                            </Link>
-                        </td>
-                        <td className="text-center p-2 m-0">
-                            <Link to="#">
-                                <DeleteForeverIcon onClick={() => { if(onRemove!=null) onRemove(order.id) }} />
-                            </Link>
-                        </td>
-                    </tr>
+                            <Col className="text-center p-2 m-0 col-2">
+                                <small className="text-muted">
+                                { formatDate(new Date(order.dateTime.startDate))} <br/> { formatDate(new Date(order.dateTime.endDate)) }
+                                </small>
+                            </Col>
+
+                            <Col className="text-center p-2 m-0 col-1">
+                                <Row>
+                                    <Col>
+                                        {curDate < new Date(order.dateTime.startDate) &&
+                                        <Link to={"/admin/orders/" + order.id}>
+                                            <EditIcon />
+                                        </Link>
+                                        }
+                                    </Col>
+                                    <Col>
+                                        <Link to="#">
+                                            <DeleteForeverIcon onClick={() => { if(onRemove!=null) onRemove(order.id) }} />
+                                        </Link>
+                                    </Col>
+                                </Row>
+                            </Col>
+                        </Row>
+                        <hr/>
+                    </div>
                     );
                 })
                 }
-                </tbody>
-            </Table>
+            </>
             }
     </>);
 };
