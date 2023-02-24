8e17c3b718e4ad2119d977c751f5b0076c0b2db2
eXetrum <shishimorovmaksim@gmail.com>
2023-02-24 21:10:48 +0200

    masters model migration

---
+++ server/database/migrations/20230224131320-create-masters.js
@@ -0,0 +1,42 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable('masters', {
+			id: {
+				type: Sequelize.DataTypes.UUID,
+				defaultValue: Sequelize.DataTypes.UUIDV4,
+				allowNull: false,
+				primaryKey: true,				
+			},
+			email: {
+				allowNull: false,
+				unique: true,
+				type: Sequelize.DataTypes.STRING
+			},
+			password: {
+				allowNull: false,
+				type: Sequelize.DataTypes.STRING
+			},
+			name: {
+				allowNull: false,
+				type: Sequelize.DataTypes.STRING
+			},
+			rating: {
+				allowNull: false,
+				type: Sequelize.DataTypes.INTEGER
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DataTypes.DATE
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DataTypes.DATE
+			}
+		});
+	},
+	async down(queryInterface, Sequelize) {
+		await queryInterface.dropTable('masters');
+	}
+};
