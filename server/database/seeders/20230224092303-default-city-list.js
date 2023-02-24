b00507b287b2acc5f03589b87ef9a1a13ffaf2b0
eXetrum <shishimorovmaksim@gmail.com>
2023-02-24 15:02:33 +0200

    City model, migration, seed

---
+++ server/database/seeders/20230224092303-default-city-list.js
@@ -0,0 +1,44 @@
+const uuid = require('uuid');
+
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+	async up (queryInterface, Sequelize) {
+		/**
+		 * Add seed commands here.
+		 *
+		 * Example:
+		 * await queryInterface.bulkInsert('People', [{
+		 *   name: 'John Doe',
+		 *   isBetaMember: false
+		 * }], {});
+		*/
+
+		return queryInterface.bulkInsert('cities', [
+			{
+				id: uuid.v4(),
+				name: 'Дніпро',
+				createdAt: new Date(),
+				updatedAt: new Date()
+			}, 
+			{
+				id: uuid.v4(),
+				name: 'Ужгород',
+				createdAt: new Date(),
+				updatedAt: new Date()
+			}
+		]);
+	},
+
+	async down (queryInterface, Sequelize) {
+		/**
+		 * Add commands to revert seed here.
+		 *
+		 * Example:
+		 * await queryInterface.bulkDelete('People', null, {});
+		*/
+		const Op = Sequelize.Op;
+		return queryInterface.bulkDelete('cities', {name: {[Op.in]: ['Дніпро', 'Ужгород']}}, {});
+	}
+};
