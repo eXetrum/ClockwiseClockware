c22c3a1bb2ba66d17af1c3971da8a0dca7fff2ca
eXetrum <shishimorovmaksim@gmail.com>
2023-02-24 14:59:35 +0200

    append to env new constants that relate to ps for prod/dev/test

--- server/models/db.js
+++ server/models/db.js
@@ -3,12 +3,12 @@ const { Pool } = require('pg');
 
 
 const pool = new Pool({
-	database: process.env.POSTGRES_DB,
-	user: process.env.POSTGRES_USER,	
-	password: process.env.POSTGRES_PASS,
-	host: process.env.POSTGRES_HOST,
-	port: process.env.POSTGRES_PORT,
-	ssl: process.env.POSTGRES_SSL === 'true'
+	database: process.env.DEV_POSTGRES_DB,
+	user: process.env.DEV_POSTGRES_USER,	
+	password: process.env.DEV_POSTGRES_PASS,
+	host: process.env.DEV_POSTGRES_HOST,
+	port: process.env.DEV_POSTGRES_PORT,
+	ssl: process.env.DEV_POSTGRES_SSL === 'true'
 });
 
 const execQuery = async (code, bind_args=null) => { 
