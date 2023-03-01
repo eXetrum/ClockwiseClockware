6ee24d3b44d65ecd62e9659b940380e01a33b5c7
eXetrum <shishimorovmaksim@gmail.com>
2023-03-02 00:27:36 +0200

    Remove

--- client/src/components/LoadingContainer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Spinner } from 'react-bootstrap';
-
-const LoadingContainer = ({ condition=false }) => {
-    if(!condition) return null;
-    return (<center><Spinner animation="grow" /></center>);
-};
-
-export default LoadingContainer;
