e37675924a325d60ba515ca40b68b167e785919e
eXetrum <shishimorovmaksim@gmail.com>
2023-03-02 00:27:43 +0200

    Update Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,19 +1,14 @@
-import React, { useEffect, useState } from 'react'
-import { Navigate, useNavigate, useLocation } from 'react-router-dom'
-import { 
-	Container, Row, Col, Form, Button, Spinner
-} from 'react-bootstrap';
-import Header from './Header';
-import ErrorServiceOffline from './ErrorServiceOffline';
-import ErrorNotFound from './ErrorNotFound';
-import { isLoggedIn, login, setToken } from '../api/auth';
-
+import React, { useState, useCallback } from 'react'
+import { Navigate, useLocation } from 'react-router-dom'
+import { Container, Row, Col, Form, Button, Spinner } from 'react-bootstrap';
 import { useSnackbar } from 'notistack';
+import Header from './Header';
+import ErrorContainer from './ErrorContainer';
+import { isLoggedIn, login, setToken } from '../api/auth';
 
 const Login = () => {
 	const { enqueueSnackbar, closeSnackbar } = useSnackbar();
 
-	const navigate = useNavigate();
 	const location = useLocation();
 	const fromPage = location.state?.from?.pathname || '/';
 
@@ -22,17 +17,21 @@ const Login = () => {
 	const [redirect, setRedirect] = useState(isLoggedIn());
 	const [error, setError] = useState(null);
 
+    const isFormValid = useCallback(() => /\w{1,}@\w{1,}\.\w{2,}/ig.test(user?.email) && user.password, [user]);
+
 	const doLogin = async (email, password) => {
 		try {
 			const response = await login(email, password);
-			if(response && response.data && response.data.accessToken) {
-				setToken(response.data.accessToken);
+			if(response?.data?.accessToken) {
+				const { accessToken } = response.data;
+				setToken(accessToken);
+				enqueueSnackbar(`Success`, { variant: 'success' });
 				setRedirect(true);
 			}
 		} catch(e) {
-			console.log("LOGIN ERROR: ", e);
+			console.log("doLogin: ", e);
 			setError(e);
-			if(e && e.response && e.response.data && e.response.data.detail) {
+			if(e?.response?.data?.detail) {
 				enqueueSnackbar(`Error: ${e.response.data.detail}`, { variant: 'error' });
 			}
 		} finally {
@@ -40,13 +39,21 @@ const Login = () => {
 		}
 	};
 
-	const handleChange = (event) => {
+	const onFormFieldChange = (event) => {
 		const inputField = event.target.name
 		const inputValue = event.target.value
 		
 		setUser((prev) => ({...prev, [inputField]: inputValue }));
-
 		setError(null);
+	};
+
+	const onFormSubmit = (event) => {
+		event.preventDefault();
+
+		setPending(true);
+		setError(null);
+		
+		doLogin({ ...user });
 	};
 
 	if(redirect) {
@@ -61,43 +68,33 @@ const Login = () => {
 				<Row className="justify-content-md-center">
 					<Col xs lg="4" md="auto">
 						<h1>Login page</h1>
-						<Form onSubmit={(event) => {
-							event.preventDefault();
-					
-							setPending(true);
-							setError(null);
-							
-							doLogin(user.email, user.password);
-						}}>
-						<Form.Group className="mb-3">
-						<Form.Label>Email address</Form.Label>
-						<Form.Control type="email" placeholder="Enter email" 
-							name="email"
-							onChange={handleChange} 
-							value={user.email}
-							disabled={pending}
-						/>
-						</Form.Group>
-						<Form.Group className="mb-3">
-						<Form.Label>Password</Form.Label>
-						<Form.Control type="password" placeholder="Password" 
-							name="password"
-							onChange={handleChange} 
-							value={user.password}
-							disabled={pending}
-						/>
-						</Form.Group>
-						<Button variant="primary" type="submit" disabled={!user.email || !user.password || pending}>
-						{pending && <Spinner className="me-2" as="span" animation="grow" size="sm" role="status" aria-hidden="true" />}
-						Login
-						</Button>                
-					</Form>
+						<Form onSubmit={onFormSubmit}>
+							<Form.Group className="mb-3">
+							<Form.Label>Email address</Form.Label>
+							<Form.Control type="email" placeholder="Enter email" name="email" autoFocus
+								onChange={onFormFieldChange}							
+								value={user.email}
+								disabled={pending}
+							/>
+							</Form.Group>
+							<Form.Group className="mb-3">
+							<Form.Label>Password</Form.Label>
+							<Form.Control type="password" placeholder="Password" name="password"
+								onChange={onFormFieldChange} 			
+								value={user.password}
+								disabled={pending}
+							/>
+							</Form.Group>
+							<Button variant="primary" type="submit" disabled={!isFormValid()}>
+							{pending && <Spinner className="me-2" as="span" animation="grow" size="sm" role="status" aria-hidden="true" />}
+							Login
+							</Button>                
+						</Form>
 					</Col>
 				</Row>
 				
 				<hr/>
-				<ErrorServiceOffline error={error} pending={pending} />
-            	<ErrorNotFound error={error} pending={pending} />
+				<ErrorContainer error={error} />
 			</Container>
       	</Container>
 	);
