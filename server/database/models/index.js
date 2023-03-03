a64b13c7d25e017d5dda1f75f61b90a062de1605
eXetrum <shishimorovmaksim@gmail.com>
2023-03-03 18:28:04 +0200

    ty console.log, you saved my life

--- server/database/models/index.js
+++ server/database/models/index.js
@@ -9,14 +9,10 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + './../config/config.js')[env];
 const db = {};
 
-console.log('sequelize index, env: ', env);
-console.log('sequelize index, cfg: ', config);
 let sequelize;
 if (config.use_env_variable) {
-	console.log('use_env_variable:true');
 	sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-	console.log('use_env_variable:false');
 	sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
