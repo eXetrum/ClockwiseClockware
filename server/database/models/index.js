49167b60f0f346b76bb963d4a8a3f101e18442e3
eXetrum <shishimorovmaksim@gmail.com>
2023-03-03 17:14:24 +0200

    Update index.js

--- server/database/models/index.js
+++ server/database/models/index.js
@@ -9,6 +9,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + './../config/config.js')[env];
 const db = {};
 
+console.log('sequelize index, env: ', env);
 let sequelize;
 if (config.use_env_variable) {
 	console.log('use_env_variable:true');
