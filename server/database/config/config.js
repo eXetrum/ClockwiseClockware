a25425dfd562fef85d7f30c15f88b0af9e1a9c47
eXetrum <shishimorovmaksim@gmail.com>
2023-03-03 17:35:06 +0200

    renderer-sequelize-ssl test-fix#1

--- server/database/config/config.js
+++ server/database/config/config.js
@@ -2,33 +2,39 @@ require('dotenv').config();
 
 module.exports = {
 	development: {
-		username: process.env.DEV_POSTGRES_USER,
-		password: process.env.DEV_POSTGRES_PASS,
-		database: process.env.DEV_POSTGRES_DB,
-		host: process.env.DEV_POSTGRES_HOST,
-		port: process.env.DEV_POSTGRES_PORT,
-		ssl: process.env.DEV_POSTGRES_SSL === 'true',
-		dialect: 'postgres'
-	},
-	test: {
-		username: process.env.TEST_POSTGRES_USER,
-		password: process.env.TEST_POSTGRES_PASS,
-		database: process.env.TEST_POSTGRES_DB,
-		host: process.env.TEST_POSTGRES_HOST,
-		port: process.env.TEST_POSTGRES_PORT,
-		ssl: process.env.TEST_POSTGRES_SSL === 'true',
-		dialect: 'postgres'
-	},
-	production: {
-		username: process.env.PROD_POSTGRES_USER,
-		password: process.env.PROD_POSTGRES_PASS,
-		database: process.env.PROD_POSTGRES_DB,
-		host: process.env.PROD_POSTGRES_HOST,
-		port: process.env.PROD_POSTGRES_PORT,
-		ssl: process.env.PROD_POSTGRES_SSL === 'true',
+		username: process.env.POSTGRES_USER,
+		password: process.env.POSTGRES_PASS,
+		database: process.env.POSTGRES_DB,
+		host: process.env.POSTGRES_HOST,
+		port: process.env.POSTGRES_PORT,
+		ssl: process.env.POSTGRES_SSL === 'true',
 		dialect: 'postgres',
 		dialectOptions: {
-			ssl: process.env.PROD_POSTGRES_SSL === 'true'
+			ssl: process.env.POSTGRES_SSL === 'true'
+		}
+	},
+	test: {
+		username: process.env.POSTGRES_USER,
+		password: process.env.POSTGRES_PASS,
+		database: process.env.POSTGRES_DB,
+		host: process.env.POSTGRES_HOST,
+		port: process.env.POSTGRES_PORT,
+		ssl: process.env.POSTGRES_SSL === 'true',
+		dialect: 'postgres',
+		dialectOptions: {
+			ssl: process.env.POSTGRES_SSL === 'true'
+		}
+	},
+	production: {
+		username: process.env.POSTGRES_USER,
+		password: process.env.POSTGRES_PASS,
+		database: process.env.POSTGRES_DB,
+		host: process.env.POSTGRES_HOST,
+		port: process.env.POSTGRES_PORT,
+		ssl: process.env.POSTGRES_SSL === 'true',
+		dialect: 'postgres',
+		dialectOptions: {
+			ssl: process.env.POSTGRES_SSL === 'true'
 		}
 	}
 };
