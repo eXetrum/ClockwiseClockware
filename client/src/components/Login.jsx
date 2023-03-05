0faf74d2a409f5677ad1ea46dd3900974fc0d7ef
eXetrum <shishimorovmaksim@gmail.com>
2023-03-05 17:04:20 +0200

    Update Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
   const [redirect, setRedirect] = useState(isLoggedIn());
   const [error, setError] = useState(null);
 
-  const isFormValid = useCallback(() => /\w{1,}@\w{1,}\.\w{2,}/gi.test(user?.email) && user.password, [user]);
+  const isFormValid = useCallback(() => /\w{1,}@\w{1,}\.\w{2,}/gi.test(user.email) && user.password, [user]);
 
   let abortController = null;
 
@@ -34,9 +34,7 @@ const Login = () => {
       }
     } catch (e) {
       setError(e);
-      if (e?.response?.data?.detail) {
-        enqueueSnackbar(`Error: ${e.response.data.detail}`, { variant: 'error' });
-      }
+      if (e?.response?.data?.detail) enqueueSnackbar(`Error: ${e.response.data.detail}`, { variant: 'error' });
     } finally {
       setPending(false);
     }
