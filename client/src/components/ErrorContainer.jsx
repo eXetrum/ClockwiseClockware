17b2ce2d5ef9b3fa3074f1b03878dc0f16993f71
eXetrum <shishimorovmaksim@gmail.com>
2023-03-05 17:04:11 +0200

    getErrorText

--- client/src/components/ErrorContainer.jsx
+++ client/src/components/ErrorContainer.jsx
@@ -1,23 +1,14 @@
 import React from 'react';
 import { Row, Col, Alert } from 'react-bootstrap';
-import CloudOffIcon from '@mui/icons-material/CloudOff';
-import ErrorOutlineOutlinedIcon from '@mui/icons-material/ErrorOutlineOutlined';
-import BlockOutlinedIcon from '@mui/icons-material/BlockOutlined';
-import SyncProblemIcon from '@mui/icons-material/SyncProblem';
+import { getIconByError, getErrorText } from '../utils/error';
 
 const ErrorContainer = ({ error = null }) => {
   if (error == null) return null;
 
-  const getIconByError = (error) => {
-    if (error?.code === 'ERR_NETWORK') return <CloudOffIcon fontSize="large" />;
-    if (error?.code === 'ERR_BAD_REQUEST' && error?.response?.status === 403) return <BlockOutlinedIcon fontSize="large" />;
-    if (error?.code === 'ERR_BAD_REQUEST' && error?.response?.status === 404) return <ErrorOutlineOutlinedIcon fontSize="large" />;
-    if (error?.code === 'ERR_BAD_REQUEST' && error?.response?.status === 400) return <SyncProblemIcon fontSize="large" />;
-    return null;
-  };
-
   const ErrorIcon = getIconByError(error);
   if (!ErrorIcon) return null;
+
+  const ErrorText = getErrorText(error);
 
   return (
     <>
@@ -26,7 +17,7 @@ const ErrorContainer = ({ error = null }) => {
           <Alert key="danger" variant="danger" className="m-0">
             {ErrorIcon}
             &nbsp;
-            {error?.response?.data?.detail?.toString() || error?.response?.statusText || error?.message || error?.toString()}
+            {ErrorText}
           </Alert>
         </Col>
       </Row>
