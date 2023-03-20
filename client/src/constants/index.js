e19be332269ea5ec8aa9221a667701dd18560fff
eXetrum <shishimorovmaksim@gmail.com>
2023-03-20 12:28:38 +0200

    Update index.js

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,22 +1,22 @@
-export const ORDER_STATUS = Object.freeze({
+export const ORDER_STATUS = {
   CONFIRMED: 'confirmed',
   COMPLETED: 'completed',
   CANCELED: 'canceled',
-});
+};
 
-export const USER_ROLES = Object.freeze({
+export const USER_ROLES = {
   ADMIN: 'admin',
   MASTER: 'master',
   CLIENT: 'client',
-});
+};
 
-export const ACCESS_SCOPE = Object.freeze({
+export const ACCESS_SCOPE = {
   AnyAuth: [USER_ROLES.ADMIN, USER_ROLES.MASTER, USER_ROLES.CLIENT],
   AdminOnly: [USER_ROLES.ADMIN],
   MasterOnly: [USER_ROLES.MASTER],
   ClientOnly: [USER_ROLES.CLIENT],
   MasterOrClient: [USER_ROLES.MASTER, USER_ROLES.CLIENT],
-});
+};
 
 export const SNACKBAR_MAX_SNACKS = 5;
 export const SNACKBAR_AUTOHIDE_TIMEOUT = 6000;
