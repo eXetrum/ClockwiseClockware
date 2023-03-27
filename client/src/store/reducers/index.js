f45526f4d3e83bac36549434a2cf75b6b0cb27c8
eXetrum <shishimorovmaksim@gmail.com>
2023-03-27 19:20:04 +0300

    Update index.js

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -1,8 +1,10 @@
+export { watchSlice } from './WatchSlice';
 export { citySlice } from './CitySlice';
 export { masterSlice } from './MasterSlice';
 export { clientSlice } from './ClientSlice';
 export { orderSlice } from './OrderSlice';
 
+export { default as watchReducer } from './WatchSlice';
 export { default as cityReducer } from './CitySlice';
 export { default as masterReducer } from './MasterSlice';
 export { default as clientReducer } from './ClientSlice';
