c9e8031601cdfb6ca4b93169f9e9e0dbd8ff2dfe
eXetrum <shishimorovmaksim@gmail.com>
2023-03-29 23:30:41 +0300

    protected route for order page

--- client/src/providers/RouteGuard.jsx
+++ client/src/providers/RouteGuard.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { useLocation, Navigate } from 'react-router-dom';
 
 import { useSelector } from 'react-redux';
-import { ACCESS_SCOPE } from '../constants';
+import { ACCESS_SCOPE, USER_ROLES } from '../constants';
 
-const RouteGuard = ({ children, scope = ACCESS_SCOPE.AnyAuth }) => {
+const RouteGuard = ({ children, scope = ACCESS_SCOPE.AnyAuth, redirectTo = '/login' }) => {
   const location = useLocation();
 
   const { authUser: auth } = useSelector(state => state.authReducer);
 
-  if (auth.user === null && location.pathname !== '/login') return <Navigate to="/login" state={{ from: location }} />;
+  if (auth.user.role === USER_ROLES.GUEST && !scope.includes(auth.user.role) && location.pathname !== redirectTo)
+    return <Navigate to={redirectTo} state={{ from: location }} />;
 
   if (!scope.includes(auth.user.role)) return <Navigate to="/" />;
 
