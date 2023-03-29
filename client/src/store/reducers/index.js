cc02ce6b4fa85e8b832257c53a5348976fa5b03c
eXetrum <shishimorovmaksim@gmail.com>
2023-03-29 16:09:53 +0300

    slice imports removed

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -1,10 +1,3 @@
-export { watchSlice } from './watchSlice';
-export { citySlice } from './citySlice';
-export { masterSlice } from './masterSlice';
-export { clientSlice } from './clientSlice';
-export { orderSlice } from './orderSlice';
-export { authSlice } from './authSlice';
-
 export { default as watchReducer } from './watchSlice';
 export { default as cityReducer } from './citySlice';
 export { default as masterReducer } from './masterSlice';
