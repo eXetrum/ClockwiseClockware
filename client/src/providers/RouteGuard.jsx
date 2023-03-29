9c6173f2e95f4c4cb4b785814bac3a22efb3b7d9
eXetrum <shishimorovmaksim@gmail.com>
2023-03-29 12:23:20 +0300

    RouteGuard using redux store auth user

--- client/src/providers/RouteGuard.jsx
+++ client/src/providers/RouteGuard.jsx
@@ -1,24 +1,17 @@
-import { useEffect, useState } from 'react';
+import React from 'react';
 import { useLocation, Navigate } from 'react-router-dom';
-import { useAuth } from '../hooks';
-import { parseToken } from '../utils';
+
+import { useSelector } from 'react-redux';
 import { ACCESS_SCOPE } from '../constants';
 
 const RouteGuard = ({ children, scope = ACCESS_SCOPE.AnyAuth }) => {
   const location = useLocation();
-  const { accessToken, setAccessToken } = useAuth();
-  const [user, setUser] = useState(parseToken(accessToken));
-  useEffect(() => {
-    const obj = parseToken(accessToken);
-    if (obj === null && accessToken !== null) setAccessToken(null);
-    setUser(obj);
-  }, [location, accessToken, setAccessToken]);
 
-  if (user === null && location.pathname !== '/login') {
-    return <Navigate to="/login" state={{ from: location }} />;
-  }
+  const { authUser: auth } = useSelector(state => state.authReducer);
 
-  if (!scope.includes(user.role)) return <Navigate to="/" />;
+  if (auth.user === null && location.pathname !== '/login') return <Navigate to="/login" state={{ from: location }} />;
+
+  if (!scope.includes(auth.user.role)) return <Navigate to="/" />;
 
   return children;
 };
