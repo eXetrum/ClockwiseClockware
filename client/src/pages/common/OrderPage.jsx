2c056941f5298f1bd928681e591897d01e12dfb8
eXetrum <shishimorovmaksim@gmail.com>
2023-03-29 23:38:43 +0300

    summary page; next, edit, create buttons

--- client/src/pages/common/OrderPage.jsx
+++ client/src/pages/common/OrderPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useMemo, useCallback } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import { Container, Row, Col, Button, Alert } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button, Alert, Spinner } from 'react-bootstrap';
 import { PuffLoader } from 'react-spinners';
 import { confirm } from 'react-bootstrap-confirmation';
 import { useSnackbar } from 'notistack';
@@ -11,7 +11,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchWatches, fetchCities, addOrder } from '../../store/thunks';
 import { resetNewOrder } from '../../store/actions/orderActions';
 
-import { ERROR_TYPE } from '../../constants';
+import { ACCESS_SCOPE, ERROR_TYPE } from '../../constants';
+import { formatDate, addHours, formatDecimal } from '../../utils';
 
 const OrderPage = () => {
   const { enqueueSnackbar } = useSnackbar();
@@ -19,20 +20,37 @@ const OrderPage = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
+  const { authUser: auth } = useSelector(state => state.authReducer);
+
   const { error: errorWatches, watches, isInitialLoading: isInitialLoadingWatches } = useSelector(state => state.watchReducer);
   const { error: errorCities, cities, isInitialLoading: isInitialLoadingCities } = useSelector(state => state.cityReducer);
 
-  const { newOrder } = useSelector(state => state.orderReducer);
+  const { newOrder, isPending } = useSelector(state => state.orderReducer);
 
   const error = errorWatches || errorCities;
 
   const [isOrderPlaced, setOrderPlaced] = useState(false);
+  const [isOrderSummary, setOrderSummary] = useState(false);
+
+  const isClientAuth = useMemo(() => ACCESS_SCOPE.ClientOnly.includes(auth.user.role), [auth]);
+  const isOrderExists = useMemo(() => location?.state?.order, [location]);
+
+  const setUpOrder = useCallback(() => {
+    if (isClientAuth && isOrderExists) {
+      dispatch(resetNewOrder({ ...location?.state?.order, client: { name: auth.user.name, email: auth.user.email } }));
+      delete location?.state?.order;
+    } else if (isClientAuth) {
+      dispatch(resetNewOrder({ client: { name: auth.user.name, email: auth.user.email } }));
+    } else {
+      dispatch(resetNewOrder(location?.state?.order));
+    }
+  }, [dispatch, location, auth, isClientAuth, isOrderExists]);
 
   useEffect(() => {
     dispatch(fetchWatches());
     dispatch(fetchCities());
-    dispatch(resetNewOrder(location?.state?.order));
-  }, [dispatch, location]);
+    setUpOrder();
+  }, [dispatch, setUpOrder]);
 
   const isInitialLoading = useMemo(
     () => isInitialLoadingWatches || isInitialLoadingCities,
@@ -43,50 +61,56 @@ const OrderPage = () => {
     [isInitialLoading, error],
   );
 
-  const onSubmit = useCallback(
-    async event => {
-      event.preventDefault();
+  const onSubmit = useCallback(async () => {
+    const order = {
+      client: newOrder.client,
+      watchId: newOrder.watch.id,
+      cityId: newOrder.city.id,
+      masterId: newOrder.master.id,
+      startDate: new Date(newOrder.startDate).getTime(),
+      timezone: new Date(newOrder.startDate).getTimezoneOffset(),
+    };
 
-      const order = {
-        client: newOrder.client,
-        watchId: newOrder.watch.id,
-        cityId: newOrder.city.id,
-        masterId: newOrder.master.id,
-        startDate: new Date(newOrder.startDate).getTime(),
-        timezone: new Date(newOrder.startDate).getTimezoneOffset(),
-      };
-
-      const action = await dispatch(addOrder(order));
-      if (isFulfilled(action)) {
-        enqueueSnackbar('Order placed', { variant: 'success' });
-        if (action.payload.autoRegistration) {
-          enqueueSnackbar('Account created, check your email for credentials', { variant: 'success' });
-        }
-        setOrderPlaced(true);
-      } else if (isRejected(action)) {
-        if (action.payload.type === ERROR_TYPE.ACCESS_DENIED) {
-          const result = await confirm(
-            `User with specified email already exists. To continue you need to login with ${newOrder.client.email} first. Proceed ?`,
-            {
-              title: 'User already exists',
-              okText: 'Yes',
-              cancelText: 'No',
-              okButtonStyle: 'success',
-            },
-          );
-          if (result) navigate('/login', { state: { from: location, order: newOrder } });
-        }
-      } else {
-        enqueueSnackbar(`Error: ${action.payload.message}`, { variant: 'error' });
+    const action = await dispatch(addOrder(order));
+    if (isFulfilled(action)) {
+      enqueueSnackbar('Order placed', { variant: 'success' });
+      if (action.payload.autoRegistration) {
+        enqueueSnackbar('Account was created! Check your email for credentials', { variant: 'success' });
       }
-    },
-    [dispatch, enqueueSnackbar, newOrder, navigate, location],
-  );
+      setOrderPlaced(true);
+    } else if (isRejected(action)) {
+      if (action.payload.type === ERROR_TYPE.ACCESS_DENIED) {
+        const result = await confirm(
+          `User with specified email already exists. To continue you need to login with ${newOrder.client.email} first. Proceed ?`,
+          {
+            title: 'User already exists',
+            okText: 'Yes',
+            cancelText: 'No',
+            okButtonStyle: 'success',
+          },
+        );
+        if (result) navigate('/login', { state: { from: location, order: newOrder } });
+      }
+    } else {
+      enqueueSnackbar(`Error: ${action.payload.message}`, { variant: 'error' });
+    }
+  }, [dispatch, enqueueSnackbar, newOrder, navigate, location]);
+
+  const onNext = useCallback(async event => {
+    event.preventDefault();
+    setOrderSummary(true);
+  }, []);
+
+  const onBack = useCallback(async event => {
+    event.preventDefault();
+    setOrderSummary(false);
+  }, []);
 
   const onReset = useCallback(async () => {
-    dispatch(resetNewOrder());
+    setUpOrder();
+    setOrderSummary(false);
     setOrderPlaced(false);
-  }, [dispatch]);
+  }, [setUpOrder]);
 
   return (
     <Container>
@@ -111,14 +135,15 @@ const OrderPage = () => {
               <>
                 <Row className="justify-content-md-center">
                   <Col md="auto">
-                    <Alert variant={'info'}>
-                      <Container>
-                        <h5 className="text-center mb-4">Thank you ! Order confirmation message was sent to your email. </h5>
-                      </Container>
+                    <Alert variant="info">
+                      <h5 className="text-center mb-4 p-1">
+                        Thank you !<br />
+                        Order confirmation message was sent to your email.
+                      </h5>
                     </Alert>
                   </Col>
                 </Row>
-                <hr />
+
                 <Row className="justify-content-md-center">
                   <Col md="auto">
                     <Button variant="primary" onClick={onReset}>
@@ -128,7 +153,121 @@ const OrderPage = () => {
                 </Row>
               </>
             ) : (
-              <OrderForm {...{ watches, cities, onSubmit, isEditForm: false, successButtonText: 'Create' }} />
+              <>
+                {isOrderSummary ? (
+                  <>
+                    <Row className="justify-content-md-center">
+                      <center>
+                        <h4>Order summary:</h4>
+                      </center>
+                      <Col xs lg="6">
+                        <hr />
+                        <Row>
+                          <Col sm={4}>
+                            <Form.Label>
+                              <b>Client:</b>
+                            </Form.Label>
+                          </Col>
+                          <Col className="d-flex justify-content-end">
+                            {newOrder.client ? (
+                              <>
+                                {newOrder.client?.name}, {newOrder.client?.email}
+                              </>
+                            ) : null}
+                          </Col>
+                        </Row>
+
+                        <hr />
+                        <Row>
+                          <Col sm={4}>
+                            <Form.Label>
+                              <b>Master:</b>
+                            </Form.Label>
+                          </Col>
+                          <Col className="d-flex justify-content-end">
+                            {newOrder.master ? (
+                              <>
+                                {newOrder.master?.name}, {newOrder.master?.email}
+                              </>
+                            ) : null}
+                          </Col>
+                        </Row>
+
+                        <hr />
+                        <Row>
+                          <Col sm={4}>
+                            <Form.Label>
+                              <b>City:</b>
+                            </Form.Label>
+                          </Col>
+                          <Col className="d-flex justify-content-end">{newOrder?.city?.name}</Col>
+                        </Row>
+
+                        <hr />
+                        <Row>
+                          <Col sm={4}>
+                            <Form.Label>
+                              <b>Watch type:</b>
+                            </Form.Label>
+                          </Col>
+                          <Col className="d-flex justify-content-end">{newOrder?.watch?.name}</Col>
+                        </Row>
+
+                        <hr />
+                        <Row>
+                          <Col sm={4}>
+                            <Form.Label>
+                              <b>Start date:</b>
+                            </Form.Label>
+                          </Col>
+                          <Col className="d-flex justify-content-end">{formatDate(newOrder?.startDate)}</Col>
+                        </Row>
+
+                        <hr />
+                        <Row>
+                          <Col sm={4}>
+                            <Form.Label>
+                              <b>End date:</b>
+                            </Form.Label>
+                          </Col>
+                          <Col className="d-flex justify-content-end">
+                            {formatDate(addHours(newOrder?.startDate, newOrder?.watch?.repairTime))}
+                          </Col>
+                        </Row>
+
+                        <hr />
+                        <Row>
+                          <Col sm={4}>
+                            <Form.Label>
+                              <b>Total Cost:</b>
+                            </Form.Label>
+                          </Col>
+                          <Col className="d-flex justify-content-end">{formatDecimal(newOrder?.city?.pricePerHour)}</Col>
+                        </Row>
+
+                        <hr />
+                        <Row className="justify-content-center mt-4">
+                          <Col className="d-flex justify-content-start">
+                            <Button className="mb-3 col-sm-5" onClick={onBack} disabled={isPending}>
+                              Edit
+                            </Button>
+                          </Col>
+                          <Col className="d-flex justify-content-end">
+                            <Button className="mb-3 col-sm-5" type="submit" onClick={onSubmit} variant="success" disabled={isPending}>
+                              {isPending && (
+                                <Spinner className="me-2" as="span" animation="grow" size="sm" role="status" aria-hidden="true" />
+                              )}
+                              <span>Create</span>
+                            </Button>
+                          </Col>
+                        </Row>
+                      </Col>
+                    </Row>
+                  </>
+                ) : (
+                  <OrderForm {...{ watches, cities, onSubmit: onNext, onReset, isEditForm: isClientAuth, successButtonText: 'Next' }} />
+                )}
+              </>
             )}
           </>
         ) : null}
