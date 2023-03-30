524f16814d93c3136a2feef346b260fa41762f45
eXetrum <shishimorovmaksim@gmail.com>
2023-03-30 20:30:54 +0300

    destructurization

--- client/src/store/reducers/orderSlice.js
+++ client/src/store/reducers/orderSlice.js
@@ -29,30 +29,30 @@ export const orderSlice = createSlice({
   name: 'order',
   initialState,
   reducers: {
-    changeVisibilityRateForm(state, action) {
-      state.isShowRateForm = action.payload;
+    changeVisibilityRateForm(state, { payload }) {
+      state.isShowRateForm = payload;
       state.newOrder = initEmptyOrder();
       state.oldOrder = initEmptyOrder();
     },
-    changeNewOrderField(state, action) {
+    changeNewOrderField(state, { payload }) {
       // Max one level depth
-      const [head, ...rest] = action.payload.name.split('.');
-      if (!rest.length) state.newOrder[head] = action.payload.value;
-      else state.newOrder[head][rest[0]] = action.payload.value;
+      const [head, ...rest] = payload.name.split('.');
+      if (!rest.length) state.newOrder[head] = payload.value;
+      else state.newOrder[head][rest[0]] = payload.value;
     },
-    resetNewOrder(state, action) {
-      if (action.payload) state.newOrder = initEmptyOrder(action.payload);
+    resetNewOrder(state, { payload }) {
+      if (payload) state.newOrder = initEmptyOrder(payload);
       else state.newOrder = initEmptyOrder(state.oldOrder);
     },
   },
   extraReducers: {
     //#region Fetch all orders
-    [fetchOrders.pending]: (state, _) => {
+    [fetchOrders.pending]: state => {
       state.isInitialLoading = true;
       state.error = initEmptyError();
     },
-    [fetchOrders.fulfilled]: (state, action) => {
-      state.orders = action.payload.map(order => {
+    [fetchOrders.fulfilled]: (state, { payload }) => {
+      state.orders = payload.map(order => {
         order.isCompleting = false;
         order.isCanceling = false;
         order.isEvaluating = false;
@@ -61,143 +61,141 @@ export const orderSlice = createSlice({
       state.isInitialLoading = false;
       state.error = initEmptyError();
     },
-    [fetchOrders.rejected]: (state, action) => {
+    [fetchOrders.rejected]: (state, { payload }) => {
       state.isInitialLoading = false;
-      state.error = action.payload;
+      state.error = payload;
     },
     //#endregion
     //#region Create new order
-    [addOrder.pending]: (state, _) => {
+    [addOrder.pending]: state => {
       state.isPending = true;
       state.error = initEmptyError();
     },
-    [addOrder.fulfilled]: (state, action) => {
-      state.orders = [{ ...action.payload, isCompleting: false, isCanceling: false, isEvaluating: false }, ...state.orders];
+    [addOrder.fulfilled]: (state, { payload }) => {
+      state.orders.unshift({ ...payload, isCompleting: false, isCanceling: false, isEvaluating: false });
       state.isPending = false;
       state.newOrder = initEmptyOrder();
       state.error = initEmptyError();
     },
-    [addOrder.rejected]: (state, action) => {
+    [addOrder.rejected]: (state, { payload }) => {
       state.isPending = false;
-      if (isGlobalErrorType(action.payload.type, [ERROR_TYPE.ACCESS_DENIED])) state.error = action.payload;
+      if (isGlobalErrorType(payload.type, [ERROR_TYPE.ACCESS_DENIED])) state.error = payload;
     },
     //#endregion
 
     //#region Delete order by id
-    [deleteOrder.pending]: (state, _) => {
+    [deleteOrder.pending]: state => {
       state.isPending = true;
       state.error = initEmptyError();
     },
-    [deleteOrder.fulfilled]: (state, action) => {
-      state.orders = state.orders.filter(order => order.id !== action.payload);
+    [deleteOrder.fulfilled]: (state, { payload }) => {
+      state.orders = state.orders.filter(order => order.id !== payload);
       state.isPending = false;
       state.error = initEmptyError();
     },
-    [deleteOrder.rejected]: (state, action) => {
+    [deleteOrder.rejected]: (state, { payload }) => {
       state.isPending = false;
 
-      if (action.payload.type === ERROR_TYPE.ENTRY_NOT_FOUND) {
-        state.orders = state.orders.filter(order => order.id !== action.payload.id);
+      if (payload.type === ERROR_TYPE.ENTRY_NOT_FOUND) {
+        state.orders = state.orders.filter(order => order.id !== payload.id);
       }
 
-      if (isGlobalErrorType(action.payload.type, [ERROR_TYPE.ENTRY_NOT_FOUND])) state.error = action.payload;
+      if (isGlobalErrorType(payload.type, [ERROR_TYPE.ENTRY_NOT_FOUND])) state.error = payload;
     },
     //#endregion
 
     //#region Get order by id
-    [fetchOrder.pending]: (state, _) => {
+    [fetchOrder.pending]: state => {
       state.isInitialLoading = true;
       state.error = initEmptyError();
       state.newOrder = initEmptyOrder();
       state.oldOrder = initEmptyOrder();
     },
-    [fetchOrder.fulfilled]: (state, action) => {
+    [fetchOrder.fulfilled]: (state, { payload }) => {
       state.error = initEmptyError();
-      state.newOrder = initEmptyOrder(action.payload);
-      state.oldOrder = initEmptyOrder(action.payload); //{ ...action.payload.client, startDate: new Date(action.payload.startDate).getTime() };
+      state.newOrder = initEmptyOrder(payload);
+      state.oldOrder = initEmptyOrder(payload);
       state.isInitialLoading = false;
     },
-    [fetchOrder.rejected]: (state, action) => {
+    [fetchOrder.rejected]: (state, { payload }) => {
       state.isInitialLoading = false;
-      state.error = action.payload;
+      state.error = payload;
     },
     //#endregion
 
     //#region Update order by id
-    [updateOrder.pending]: (state, _) => {
+    [updateOrder.pending]: state => {
       state.isPending = true;
       state.error = initEmptyError();
     },
-    [updateOrder.fulfilled]: (state, action) => {
+    [updateOrder.fulfilled]: (state, { payload }) => {
       state.isPending = false;
       state.error = initEmptyError();
-      state.newOrder = action.payload;
-      state.oldOrder = action.payload;
+      state.newOrder = payload;
+      state.oldOrder = payload;
     },
-    [updateOrder.rejected]: (state, action) => {
+    [updateOrder.rejected]: (state, { payload }) => {
       state.isPending = false;
       state.newOrder = state.oldOrder;
-      if (isGlobalErrorType(action.payload.type, [ERROR_TYPE.BAD_REQUEST])) state.error = action.payload;
+      if (isGlobalErrorType(payload.type, [ERROR_TYPE.BAD_REQUEST])) state.error = payload;
     },
     //#endregion
 
     //#region complete order by id
-    [completeOrder.pending]: (state, action) => {
-      const orderId = action.meta.arg;
-      const idx = state.orders.map(order => order.id).indexOf(orderId);
+    [completeOrder.pending]: (state, { meta }) => {
+      const idx = state.orders.map(order => order.id).indexOf(meta.arg);
       state.orders[idx].isCompleting = true;
       state.error = initEmptyError();
     },
-    [completeOrder.fulfilled]: (state, action) => {
-      const idx = state.orders.map(order => order.id).indexOf(action.payload);
+    [completeOrder.fulfilled]: (state, { payload }) => {
+      const idx = state.orders.map(order => order.id).indexOf(payload);
       state.orders[idx].isCompleting = false;
       state.orders[idx].status = ORDER_STATUS.COMPLETED;
       state.error = initEmptyError();
     },
-    [completeOrder.rejected]: (state, action) => {
-      const idx = state.orders.map(order => order.id).indexOf(action.payload.id);
+    [completeOrder.rejected]: (state, { payload }) => {
+      const idx = state.orders.map(order => order.id).indexOf(payload.id);
       state.orders[idx].isCompleting = false;
-      if (action.payload.type === ERROR_TYPE.ENTRY_NOT_FOUND) state.orders.filter(order => order.id !== action.payload.id);
+      if (payload.type === ERROR_TYPE.ENTRY_NOT_FOUND) state.orders.filter(order => order.id !== payload.id);
     },
     //#endregion
 
     //#region cancel order by id
-    [cancelOrder.pending]: (state, action) => {
-      const orderId = action.meta.arg;
-      const idx = state.orders.map(order => order.id).indexOf(orderId);
+    [cancelOrder.pending]: (state, { meta }) => {
+      const idx = state.orders.map(order => order.id).indexOf(meta.arg);
       state.orders[idx].isCanceling = true;
       state.error = initEmptyError();
     },
-    [cancelOrder.fulfilled]: (state, action) => {
-      const idx = state.orders.map(order => order.id).indexOf(action.payload);
+    [cancelOrder.fulfilled]: (state, { payload }) => {
+      const idx = state.orders.map(order => order.id).indexOf(payload);
       state.orders[idx].isCanceling = false;
       state.orders[idx].status = ORDER_STATUS.CANCELED;
       state.error = initEmptyError();
     },
-    [cancelOrder.rejected]: (state, action) => {
+    [cancelOrder.rejected]: (state, { payload }) => {
       state.isPending = false;
-      const idx = state.orders.map(order => order.id).indexOf(action.payload.id);
+      const idx = state.orders.map(order => order.id).indexOf(payload.id);
       state.orders[idx].isCanceling = false;
-      if (action.payload.type === ERROR_TYPE.ENTRY_NOT_FOUND) state.orders.filter(order => order.id !== action.payload.id);
+      if (payload.type === ERROR_TYPE.ENTRY_NOT_FOUND) state.orders.filter(order => order.id !== payload.id);
     },
     //#endregion
 
     //#region rate order by id
-    [rateOrder.pending]: (state, action) => {
-      const idx = state.orders.map(order => order.id).indexOf(action.meta.arg.id);
+    [rateOrder.pending]: (state, { meta }) => {
+      const idx = state.orders.map(order => order.id).indexOf(meta.arg.id);
       state.orders[idx].isEvaluating = true;
       state.error = initEmptyError();
     },
-    [rateOrder.fulfilled]: (state, action) => {
-      const idx = state.orders.map(order => order.id).indexOf(action.payload.id);
+    [rateOrder.fulfilled]: (state, { payload }) => {
+      const idx = state.orders.map(order => order.id).indexOf(payload.id);
       state.orders[idx].isEvaluating = false;
-      state.orders[idx].rating = action.payload.rating;
+      state.orders[idx].rating = payload.rating;
       state.error = initEmptyError();
     },
-    [rateOrder.rejected]: (state, action) => {
-      const idx = state.orders.map(order => order.id).indexOf(action.payload.id);
+    [rateOrder.rejected]: (state, { payload }) => {
+      const idx = state.orders.map(order => order.id).indexOf(payload.id);
       state.orders[idx].isEvaluating = false;
-      if (action.payload.type === ERROR_TYPE.ENTRY_NOT_FOUND) state.orders.filter(order => order.id !== action.payload.id);
+      if (payload.type === ERROR_TYPE.ENTRY_NOT_FOUND) state.orders.filter(order => order.id !== payload.id);
     },
     //#endregion
   },
