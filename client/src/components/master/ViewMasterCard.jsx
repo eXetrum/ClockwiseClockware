37dc44c9e415fd661f5184131e0473f0b55856a8
eXetrum <shishimorovmaksim@gmail.com>
2023-03-30 13:55:10 +0300

    master card styles

--- client/src/components/master/ViewMasterCard.jsx
+++ client/src/components/master/ViewMasterCard.jsx
@@ -6,14 +6,23 @@ const ViewMasterCard = ({ master }) => {
   if (master == null) return;
 
   return (
-    <Card className="mb-3 justify-content-md-center" style={{ width: '18rem' }}>
+    <Card
+      className="d-flex justify-content-center"
+      style={{
+        width: '18rem',
+        minHeight: '12rem',
+        border: 'solid 1px #ccc',
+        color: '#333',
+        borderRadius: '20px 20px 20px 20px',
+      }}
+    >
       <Card.Body>
         <Card.Title>{master.name}</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">{master.email}</Card.Subtitle>
         <StarRating value={master.rating} readonly={true} />
         <Card.Text>
           {master.cities.map(city => (
-            <Badge bg="info" className="p-2 m-1" key={city.id}>
+            <Badge bg="info" className="p-2 m-1" pill={true} key={city.id}>
               {city.name}
             </Badge>
           ))}
