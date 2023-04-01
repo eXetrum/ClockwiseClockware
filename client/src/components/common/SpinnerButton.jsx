69d75b1b73ec54f190b07b60e07fea8f4ba04554
eXetrum <shishimorovmaksim@gmail.com>
2023-04-01 19:37:51 +0300

    Spinner button default props

--- client/src/components/common/SpinnerButton.jsx
+++ client/src/components/common/SpinnerButton.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button, Spinner } from 'react-bootstrap';
 
-const SpinnerButton = ({ onClick, text, disabled, loading = false, ...props }) => {
+const SpinnerButton = ({ onClick, text = 'Button', disabled = false, loading = false, ...props }) => {
   return (
     <Button variant="primary" type="submit" disabled={disabled} onClick={onClick} {...props}>
       {loading ? <Spinner className="me-2" as="span" animation="grow" size="sm" role="status" aria-hidden="true" /> : null}
