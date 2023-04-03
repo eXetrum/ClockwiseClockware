0f82d963ac49b30a62e125cc33dc4c3111c1cd21
eXetrum <shishimorovmaksim@gmail.com>
2023-04-03 20:12:19 +0300

    orders bootstrap table => mui  table

--- client/src/components/admin/AdminOrdersList.jsx
+++ client/src/components/admin/AdminOrdersList.jsx
@@ -1,27 +1,37 @@
 import React, { useCallback } from 'react';
-import { Link } from 'react-router-dom';
-import { Container, Row, Col, Table, Alert, Badge, Button, Spinner } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { Row, Col, Alert } from 'react-bootstrap';
 import { confirm } from 'react-bootstrap-confirmation';
 import { useSnackbar } from 'notistack';
-import Stack from '@mui/material/Stack';
+import { DataGrid, GridActionsCellItem } from '@mui/x-data-grid';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import EditIcon from '@mui/icons-material/Edit';
-import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
-//import StarRating from '../common/StarRating';
-import Rating from '@mui/material/Rating';
+import TaskAltIcon from '@mui/icons-material/TaskAlt';
+import DoNotDisturbAltIcon from '@mui/icons-material/DoNotDisturbAlt';
+
+//import React, { useCallback } from 'react';
+//import { Link } from 'react-router-dom';
+//import { Container, Row, Col, Table, Alert, Badge, Button, Spinner } from 'react-bootstrap';
+//import { confirm } from 'react-bootstrap-confirmation';
+//import { useSnackbar } from 'notistack';
+//import Stack from '@mui/material/Stack';
+//import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+//import EditIcon from '@mui/icons-material/Edit';
+//import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+
+//import Rating from '@mui/material/Rating';
 
 import { isFulfilled, isRejected } from '@reduxjs/toolkit';
 import { useDispatch } from 'react-redux';
 import { deleteOrder, completeOrder, cancelOrder } from '../../store/thunks';
 
 import { formatDate, formatDecimal } from '../../utils';
-import { ORDER_STATUS } from '../../constants';
-
-const COLUMN_HEADERS = ['id', 'Client', 'Master', 'City', 'Clock', 'Date Start', 'Date End', 'Status', 'Total Cost'];
+import { MAX_RATING_VALUE, ORDER_STATUS } from '../../constants';
 
 const AdminOrdersList = ({ orders }) => {
   const { enqueueSnackbar } = useSnackbar();
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const onRemove = useCallback(
     async order => {
@@ -71,126 +81,123 @@ const AdminOrdersList = ({ orders }) => {
     [dispatch, enqueueSnackbar],
   );
 
+  const columns = [
+    {
+      field: 'client.email',
+      headerName: 'Client Email',
+      valueGetter: params => params.row.client.email,
+    },
+    {
+      field: 'client.name',
+      headerName: 'Client Name',
+      valueGetter: params => params.row.client.name,
+    },
+    {
+      field: 'master.email',
+      headerName: 'Master Email',
+      valueGetter: params => params.row.master.email,
+    },
+    {
+      field: 'master.name',
+      headerName: 'Master Name',
+      valueGetter: params => params.row.master.name,
+    },
+    {
+      field: 'master.rating',
+      headerName: 'Master Rating',
+      type: 'number',
+      valueGetter: params => `${formatDecimal(params.row.master.rating, 1)}/${formatDecimal(MAX_RATING_VALUE, 1)}`,
+    },
+    {
+      field: 'city',
+      headerName: 'City',
+      valueGetter: params => params.row.city.name,
+    },
+    {
+      field: 'watch',
+      headerName: 'Clock',
+      valueFormatter: ({ value }) => `${value.name} (${value.repairTime}h)`,
+    },
+    {
+      field: 'startDate',
+      headerName: 'Date Start',
+      width: 140,
+      type: 'dateTime',
+      valueFormatter: ({ value }) => formatDate(value),
+    },
+    {
+      field: 'endDate',
+      headerName: 'End Start',
+      width: 140,
+      type: 'dateTime',
+      valueFormatter: ({ value }) => formatDate(value),
+    },
+    {
+      field: 'status',
+      headerName: 'Status',
+    },
+    {
+      field: 'totalCost',
+      headerName: 'Total Cost',
+      type: 'number',
+      valueFormatter: ({ value }) => formatDecimal(value),
+    },
+    {
+      field: 'actions',
+      headerName: 'actions',
+      type: 'actions',
+      width: 100,
+      disableReorder: true,
+      getActions: params => {
+        const actions = [
+          <GridActionsCellItem
+            icon={<EditIcon />}
+            label="Edit"
+            onClick={() => navigate(`/admin/orders/${params.row.id}`)}
+            disabled={params.row.status !== ORDER_STATUS.CONFIRMED}
+            showInMenu
+          />,
+          <GridActionsCellItem icon={<DeleteForeverIcon />} label="Delete" onClick={async () => onRemove(params.row)} showInMenu />,
+        ];
+        if (params.row.status === ORDER_STATUS.CONFIRMED) {
+          actions.unshift(
+            <GridActionsCellItem
+              icon={<DoNotDisturbAltIcon />}
+              label="Cancel"
+              onClick={async () => onCancel(params.row)}
+              disabled={params.row.isCompleting || params.row.isCanceling}
+              showInMenu
+            />,
+          );
+          actions.unshift(
+            <GridActionsCellItem
+              icon={<TaskAltIcon />}
+              label="Complete"
+              onClick={async () => onComplete(params.row)}
+              disabled={params.row.isCompleting || params.row.isCanceling}
+              showInMenu
+            />,
+          );
+        }
+
+        return actions;
+      },
+    },
+  ];
+
   if (orders.length === 0) {
     return (
-      <Container>
-        <Row className="justify-content-md-center mt-3">
-          <Col md="auto">
-            <Alert variant="warning">No records yet</Alert>
-          </Col>
-        </Row>
-      </Container>
+      <Row className="justify-content-md-center mt-3">
+        <Col md="auto">
+          <Alert variant="warning" className="text-center">
+            No records yet
+          </Alert>
+        </Col>
+      </Row>
     );
   }
 
-  return (
-    <Container>
-      <Table striped bordered responsive size="sm" className="mt-3">
-        <thead>
-          <tr>
-            {COLUMN_HEADERS.map(header => (
-              <th key={header} className="text-center p-2 m-0">
-                {header}
-              </th>
-            ))}
-            <th colSpan="2" className="text-center p-2 m-0"></th>
-          </tr>
-        </thead>
-        <tbody>
-          {orders.map(order => (
-            <tr key={order.id} className="m-0">
-              <td className="text-center p-2 m-0 col-1">{order.id}</td>
-              <td className="text-center p-2 m-0">
-                <p>
-                  <a className="text-muted" href={`mailto:${order.client.email}`} target="_blank" rel="noreferrer noopener">
-                    <EmailOutlinedIcon />
-                  </a>
-                  <span>{`${order.client.name}`}</span>
-                </p>
-                <small className="text-muted">{order.client.email}</small>
-              </td>
-              <td className="text-center p-2 m-0">
-                <p>
-                  <a className="text-muted" href={`mailto:${order.master.email}`} target="_blank" rel="noreferrer noopener">
-                    <EmailOutlinedIcon />
-                  </a>
-                  <span>{order.master.name}</span>
-                </p>
-                <small className="text-muted">{order.master.email}</small>
-                <div className="text-center p-2 m-0">
-                  <Rating value={order.master.rating} readOnly={true} />
-                </div>
-              </td>
-              <td className="text-center p-2 m-0">
-                <Badge className="p-2">{order.city.name}</Badge>
-              </td>
-              <td className="text-center p-2 m-0">
-                <Badge className="p-2" bg="info">
-                  {order.watch.name}
-                  <Badge pill bg="secondary" className="ms-2">
-                    {order.watch.repairTime}h
-                  </Badge>
-                </Badge>
-              </td>
-              <td className="text-center p-2 m-0">
-                <small className="text-muted">{formatDate(order.startDate)}</small>
-              </td>
-              <td className="text-center p-2 m-0">
-                <small className="text-muted">{formatDate(order.endDate)}</small>
-              </td>
-              <td className="text-center p-2 m-0">
-                <small className="text-muted">{order.status}</small>
-                {order.status === ORDER_STATUS.CONFIRMED ? (
-                  <Stack spacing={1}>
-                    <Button
-                      onClick={() => onComplete(order)}
-                      size="sm"
-                      disabled={order.isCompleting || order.isCanceling}
-                      variant="success"
-                    >
-                      {order.isCompleting ? (
-                        <Spinner className="me-2" as="span" animation="grow" size="sm" role="status" aria-hidden="true" />
-                      ) : null}
-                      Complete
-                    </Button>
-                    <Button
-                      onClick={() => onCancel(order)}
-                      size="sm"
-                      disabled={order.isCompleting || order.isCanceling}
-                      variant="secondary"
-                    >
-                      {order.isCanceling ? (
-                        <Spinner className="me-2" as="span" animation="grow" size="sm" role="status" aria-hidden="true" />
-                      ) : null}
-                      Cancel
-                    </Button>
-                  </Stack>
-                ) : null}
-              </td>
-              <td className="text-center p-2 m-0">
-                <small className="text-muted">{formatDecimal(order.totalCost)}</small>
-              </td>
-
-              <td className="text-center p-2 m-0">
-                {order.status === ORDER_STATUS.CONFIRMED ? (
-                  <Link to={'/admin/orders/' + order.id}>
-                    <EditIcon />
-                  </Link>
-                ) : (
-                  <EditIcon sx={{ color: 'gray' }} />
-                )}
-              </td>
-              <td className="text-center p-2 m-0">
-                <Link to="#">
-                  <DeleteForeverIcon onClick={() => onRemove(order)} />
-                </Link>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-    </Container>
-  );
+  return <DataGrid rows={orders} columns={columns} rowsPerPageOptions={[]} hideFooter={true} autoHeight />;
 };
 
 export default AdminOrdersList;
