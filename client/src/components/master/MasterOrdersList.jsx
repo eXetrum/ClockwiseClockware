1772d1bd4449598295431c7e08378d993e61b0a9
eXetrum <shishimorovmaksim@gmail.com>
2023-04-03 21:09:03 +0300

    master orders dashboard => mui table

--- client/src/components/master/MasterOrdersList.jsx
+++ client/src/components/master/MasterOrdersList.jsx
@@ -1,9 +1,9 @@
 import React, { useCallback } from 'react';
-import { Container, Row, Col, Table, Alert } from 'react-bootstrap';
+import { Row, Col, Alert } from 'react-bootstrap';
 import { confirm } from 'react-bootstrap-confirmation';
 import { useSnackbar } from 'notistack';
-import Stack from '@mui/material/Stack';
-import { SpinnerButton } from '../common';
+import { DataGrid, GridActionsCellItem } from '@mui/x-data-grid';
+import TaskAltIcon from '@mui/icons-material/TaskAlt';
 
 import { isFulfilled, isRejected } from '@reduxjs/toolkit';
 import { useDispatch } from 'react-redux';
@@ -12,8 +12,6 @@ import { completeOrder } from '../../store/thunks';
 
 import { formatDate, formatDecimal } from '../../utils';
 import { ORDER_STATUS } from '../../constants';
-
-const COLUMN_HEADERS = ['Client Name', 'Service', 'City', 'Date Start', 'Date End', 'Total Cost', 'Status'];
 
 const MasterOrdersList = ({ orders }) => {
   const { enqueueSnackbar } = useSnackbar();
@@ -36,68 +34,65 @@ const MasterOrdersList = ({ orders }) => {
     [dispatch, enqueueSnackbar],
   );
 
+  const columns = [
+    { field: 'client.name', headerName: 'Client Name', width: 240, valueGetter: params => params.row.client.name },
+    { field: 'watch', headerName: 'Service', valueGetter: params => params.row.watch.name },
+    { field: 'city', headerName: 'City', width: 200, valueGetter: params => params.row.city.name },
+    {
+      field: 'startDate',
+      headerName: 'Date Start',
+      width: 140,
+      type: 'dateTime',
+      valueFormatter: ({ value }) => formatDate(value),
+    },
+    {
+      field: 'endDate',
+      headerName: 'End Start',
+      width: 140,
+      type: 'dateTime',
+      valueFormatter: ({ value }) => formatDate(value),
+    },
+    {
+      field: 'status',
+      headerName: 'Status',
+    },
+    {
+      field: 'totalCost',
+      headerName: 'Total Cost',
+      type: 'number',
+      valueFormatter: ({ value }) => formatDecimal(value),
+    },
+    {
+      field: 'actions',
+      headerName: 'actions',
+      type: 'actions',
+      getActions: params => {
+        return [
+          <GridActionsCellItem
+            icon={<TaskAltIcon />}
+            label="Complete"
+            onClick={() => onComplete(params.row)}
+            disabled={params.row.isCompleting || params.row.status !== ORDER_STATUS.CONFIRMED}
+            showInMenu
+          />,
+        ];
+      },
+    },
+  ];
+
   if (orders.length === 0) {
     return (
-      <Container>
-        <Row className="justify-content-md-center mt-3">
-          <Col md="auto">
-            <Alert variant="warning">No records yet</Alert>
-          </Col>
-        </Row>
-      </Container>
+      <Row className="justify-content-md-center mt-3">
+        <Col md="auto">
+          <Alert variant="warning" className="text-center">
+            No records yet
+          </Alert>
+        </Col>
+      </Row>
     );
   }
 
-  return (
-    <Container>
-      <Table striped bordered responsive size="sm" className="mt-3">
-        <thead>
-          <tr>
-            {COLUMN_HEADERS.map(header => (
-              <th key={header} className="text-center p-2 m-0">
-                {header}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {orders.map(order => (
-            <tr key={order.id} className="m-0">
-              <td className="text-center p-2 m-0">{`${order.client.name}`}</td>
-              <td className="text-center p-2 m-0">
-                Repair clock of <b>{order.watch.name}</b> type
-              </td>
-              <td className="text-center p-2 m-0">{order.city.name}</td>
-              <td className="text-center p-2 m-0">
-                <small className="text-muted">{formatDate(order.startDate)}</small>
-              </td>
-              <td className="text-center p-2 m-0">
-                <small className="text-muted">{formatDate(order.endDate)}</small>
-              </td>
-              <td className="text-center p-2 m-0">
-                <small className="text-muted">{formatDecimal(order.totalCost)}</small>
-              </td>
-              <td className="text-center p-2 m-0">
-                <Stack spacing={1}>
-                  <small className="text-muted">{order.status}</small>
-                  {order.status === ORDER_STATUS.CONFIRMED ? (
-                    <SpinnerButton
-                      onClick={() => onComplete(order)}
-                      size="sm"
-                      variant="success"
-                      disabled={order.isCompleting}
-                      loading={order.isCompleting}
-                      text={'Complete'}
-                    />
-                  ) : null}
-                </Stack>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-    </Container>
-  );
+  return <DataGrid rows={orders} columns={columns} rowsPerPageOptions={[]} hideFooter={true} autoHeight />;
 };
 
 export default MasterOrdersList;
