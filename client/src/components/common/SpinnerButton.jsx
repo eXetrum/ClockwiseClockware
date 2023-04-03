5ea0117de6140977a5fe8b6d3c03f6f660a4a7bf
eXetrum <shishimorovmaksim@gmail.com>
2023-04-03 14:30:24 +0300

    Update SpinnerButton.jsx

--- client/src/components/common/SpinnerButton.jsx
+++ client/src/components/common/SpinnerButton.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Button, Spinner } from 'react-bootstrap';
 
-const SpinnerButton = ({ onClick = null, text = 'Button', disabled = false, loading = false, ...props }) => {
+const SpinnerButton = ({ text = 'Button', loading = false, ...props }) => {
   return (
-    <Button variant="primary" type="submit" disabled={disabled} onClick={onClick} {...props}>
+    <Button variant="primary" type="submit" {...props}>
       {loading ? <Spinner className="me-2" as="span" animation="grow" size="sm" role="status" aria-hidden="true" /> : null}
       {text}
     </Button>
