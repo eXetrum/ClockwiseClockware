e5ca180008ff2a1be67e71bb0bd6e69a435bafba
eXetrum <shishimorovmaksim@gmail.com>
2023-04-04 21:42:11 +0300

    mui image list

---
+++ client/src/components/common/OrderImageList.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+
+const OrderImageList = ({ images }) => {
+  if (images === null || images === undefined || !images.length) return null;
+
+  return (
+    <ImageList cols={3}>
+      {images.map((item, index) => {
+        return (
+          <ImageListItem
+            id={index}
+            key={index}
+            sx={{ width: 340, height: 340, border: 'solid 1px #ccc', borderRadius: '8px' }}
+            className="mt-3"
+          >
+            <img src={`${item.url}`} srcSet={`${item.url}`} alt={item.name} style={{ borderRadius: '7px', padding: '0' }} />
+          </ImageListItem>
+        );
+      })}
+    </ImageList>
+  );
+};
+
+export default OrderImageList;
