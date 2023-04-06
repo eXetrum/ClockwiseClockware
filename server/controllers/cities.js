86cf4f18bc27982b69b8f505bac9500b074bd695
eXetrum <shishimorovmaksim@gmail.com>
2023-04-06 21:25:35 +0300

    optional offset, limit params for GET cities route

--- server/controllers/cities.js
+++ server/controllers/cities.js
@@ -1,17 +1,28 @@
-const { body, param, validationResult } = require('express-validator');
+const { body, param, query, validationResult } = require('express-validator');
 const { City } = require('../database/models');
 const { RequireAuth } = require('../middleware/RouteProtector');
 const { isDbErrorEntryNotFound, isDbErrorEntryAlreadyExists, isDbErrorEntryReferences } = require('../utils');
 const { ACCESS_SCOPE } = require('../constants');
 
-const getAll = async (req, res) => {
-    try {
-        const cities = await City.findAll({ order: [['createdAt', 'DESC']] });
-        res.status(200).json({ cities }).end();
-    } catch (error) {
-        res.status(500).json(error).end();
+const getAll = [
+    query('offset', 'offset value is incorrect').optional().isInt({ min: 0 }),
+    query('limit', 'limit value is incorrect ').optional().isInt({ min: 0 }),
+    async (req, res) => {
+        try {
+            const errors = validationResult(req).array();
+            if (errors && errors.length) return res.status(400).json({ message: errors[0].msg }).end();
+
+            const { offset = 0, limit } = req.query;
+
+            const cities = await City.findAll({ order: [['createdAt', 'DESC']], limit, offset });
+            const count = await City.count();
+
+            res.status(200).json({ cities, total: count }).end();
+        } catch (error) {
+            res.status(500).json(error).end();
+        }
     }
-};
+];
 
 const create = [
     RequireAuth(ACCESS_SCOPE.AdminOnly),
@@ -41,8 +52,9 @@ const create = [
             if (errors && errors.length) return res.status(400).json({ message: errors[0].msg }).end();
 
             const { name, pricePerHour } = req.body.city;
+            console.log('city params: ', name, pricePerHour, Number(pricePerHour));
 
-            const city = await City.create({ name: name.trim(), pricePerHour });
+            const city = await City.create({ name: name.trim(), pricePerHour: Number(pricePerHour) });
             res.status(201).json({ city }).end();
         } catch (error) {
             if (isDbErrorEntryAlreadyExists(error)) return res.status(409).json({ message: 'City already exists' }).end();
